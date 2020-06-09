d7b74a551a802b146530800188a0ee2121cdc927
Jacob Wunder <jacob@jacobwunder.com>
2020-06-09 14:06:16 -0400

    some updates

---
+++ precache-manifest.ef5c04bb38df99c1b0cad3aad6e230fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdeb772cc329050cebb2a1016f6ebaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd794595f3ee7c879a8d",
+    "url": "/static/css/main.0336b9f5.chunk.css"
+  },
+  {
+    "revision": "2bad9c38d20843a67f6c",
+    "url": "/static/js/2.77ec7bc1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.77ec7bc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd794595f3ee7c879a8d",
+    "url": "/static/js/main.ee038546.chunk.js"
+  },
+  {
+    "revision": "fd103d41066edfe2edd7",
+    "url": "/static/js/runtime-main.06363229.js"
+  }
+]);
